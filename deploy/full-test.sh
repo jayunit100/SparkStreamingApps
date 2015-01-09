1d348fb9bab460f4814e3621084841d55c33c409
jayunit100 <jayunit100@gmail.com>
2015-01-09 16:06:14 -0500

    full test of everything

---
+++ deploy/full-test.sh
@@ -0,0 +1,25 @@
+echo "WARNING REMOVING ALL CONTAINERS in 5 SECONDS !"
+
+sleep 5
+
+# Remove all containers. 
+
+docker rm -f `docker ps --no-trunc -aq` 
+
+echo "NOW RESTARTING DOCKER !" 
+
+service docker restart 
+
+echo "NOW CREATING VAGRANT DOCKER CLUSTER "
+
+vagrant destroy --force && vagrant up --no-parallel
+
+## Run calculate pi. 
+
+docker exec -i -t scale2 \
+/opt/spark-1.2.0-bin-hadoop2.4/bin/spark-submit \
+--class org.apache.spark.examples.SparkPi \
+--master spark://scale1.docker:7077 /scale-shared/spark-examples_2.10-1.1.1.jar 100
+
+
+echo "DONE TESTING .  RESULTS ^ " 
