b303b628f5e24ff7bc99b2e6863401f995bacbb7
root <root@host18-rack11.scale.openstack.engineering.redhat.com>
2015-01-19 22:57:04 +0000

    WIP : Kuberization

--- deploy/spark/run.sh
+++ deploy/spark/run.sh
@@ -1,18 +1,31 @@
 #/bin/bash
+    
+if [ $# -eq 0 ]; then
+        echo "No arguments supplied"
+	echo "USAGE ./run.sh master|slave"
+        exit 1
+fi   
+ 
 ### Vagrant : This is untested, but it should generally work.
 if [[ `hostname -s` =~ scale[0-9]*docker ]]; then
-    if [[ `hostname` = 'scale1.docker' ]] ; then 
+    if [ $1 = "master" ] ; then 
         /opt/spark-1.2.0-bin-hadoop2.4/sbin/start-master.sh ; 
-    else 
+    # slave
+    else
         ping -c 2 scale1.docker && /opt/spark-1.2.0-bin-hadoop2.4/sbin/start-slave.sh -h spark://scale1.docker:7077 ; 
     fi 
+
 ### Kubernetes
 else
-    if [ -n "${SPARK_MASTER_HOST+1}" ]; then
-       echo "START SLAVE pointing to ${SPARK_MASTER_HOST} " 
-       /opt/spark-1.2.0-bin-hadoop2.4/sbin/start-slave.sh -h spark://${SPARK_MASTER_HOST}:7077        
+   
+    # One arg expected when launching kubernetes (master or slave)
+    if [ $1 = "slave" ]; then 
+       echo "START SLAVE pointing to ${SPARK_MASTER_SERVICE_HOST} " 
+       /opt/spark-1.2.0-bin-hadoop2.4/sbin/start-slave.sh -h spark://${SPARK_MASTER_SERVICE_HOST}:7077
+    # master
     else 
        /opt/spark-1.2.0-bin-hadoop2.4/sbin/start-master.sh
+   
     fi
 fi
 ### In all cases, we tail the logs as the final task...
