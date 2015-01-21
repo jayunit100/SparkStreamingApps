aa0d6aaa762d7762ee2405fb39000580029e294a
root <root@host18-rack11.scale.openstack.engineering.redhat.com>
2015-01-21 23:44:22 +0000

    remove old kb files

--- deploy/kube-deploy.sh
+++
@@ -1,8 +0,0 @@
-# Below we start the master pod and service first.
-# Then, we start slaves, which will connect through the 
-# slave proxy. 
-kubectl create -f ./spark-master-pod.yaml 
-kubectl create -f ./spark-master-service.json 
-kubectl create -f ./spark-slave-controller.json 
-kubectl create -f ./spark-slave-service.json 
-kubectl get pods,services
--- deploy/run-kube-startup.sh
+++
@@ -1,4 +0,0 @@
-
-for SERVICES in kube-proxy kubelet docker; do      systemctl restart docker;     systemctl enable docker;     systemctl status -l docker ; done
-
-for serv  in etcd kube-apiserver kube-controller-manager kube-scheduler; do systemctl restart kube-scheduler; systemctl enable kube-scheduler; systemctl status kube-scheduler; done
