2780944a1ebbbc20ead8f8c20df0f15ee8c33de2
jayunit100 <jayunit100@gmail.com>
2015-02-10 15:30:25 -0500

    Fixes to get pure vagrant microservices working again

--- deploy/spark/run.sh
+++ deploy/spark/run.sh
@@ -1,18 +1,10 @@
 #/bin/bash
-    
-if [ $# -eq 0 ]; then
-        echo "No arguments supplied"
-	echo "USAGE ./run.sh master|slave"
-        exit 1
-fi   
- 
-echo "running docker spark startup"
-
+echo "starting ........"
 if [ $1 = "master" ] ; then 
-        /opt/spark-1.2.0-bin-hadoop2.4/sbin/start-master.sh ; 
-# slave
+    /opt/spark-1.2.0-bin-hadoop2.4/sbin/start-master.sh ; 
 else
-        ping -c 2 scale1.docker && /opt/spark-1.2.0-bin-hadoop2.4/sbin/start-slave.sh -h spark://scale1.docker:7077 ;  
-fi
-### In all cases, we tail the logs as the final task...
-tailf /opt/spark-1.2.0-bin-hadoop2.4/logs/*
+    echo "pinging slave........"
+    ping -c 2 scale1.docker && /opt/spark-1.2.0-bin-hadoop2.4/sbin/start-slave.sh -h spark://scale1.docker:7077 ; 
+fi 
+
+tail -f /opt/spark-1.2.0-bin-hadoop2.4/logs/*
--- deploy/vagrant-run-up-test.sh
+++ deploy/vagrant-run-up-test.sh
@@ -20,7 +20,7 @@ echo "RUNNING smoke tests..."
 
 
 if `sleep 2 && docker ps | grep -q scale1`; then
-    docker exec -i -t scale1 /opt/spark-1.2.0-bin-hadoop2.4/bin/spark-submit --class org.apache.spark.examples.SparkPi --master spark://scale1.docker:7077 /scale-shared/spark-examples_2.10-1.1.1.jar 10000
+    docker exec -i -t scale1 /opt/spark-1.2.0-bin-hadoop2.4/bin/spark-submit --class org.apache.spark.examples.SparkPi --master spark://scale1.docker:7077 /scale-shared/spark-examples_2.10-1.1.1.jar 100
 else
     echo "FAILED: Looks like vagrant creation of the spark containers failed ..."
     docker ps
