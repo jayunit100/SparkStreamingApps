abd8cf1b78c87852fe1bd1bd28612fca860d08e9
jayunit100 <jayunit100@gmail.com>
2015-02-10 15:16:40 -0500

    cleaned up

--- deploy/spark/run.sh
+++ deploy/spark/run.sh
@@ -6,28 +6,13 @@ if [ $# -eq 0 ]; then
         exit 1
 fi   
  
-### Vagrant : This is untested, but it should generally work.
-if [[ `hostname -s` =~ scale[0-9]*docker ]]; then
-    if [ $1 = "master" ] ; then 
-        /opt/spark-1.2.0-bin-hadoop2.4/sbin/start-master.sh ; 
-    # slave
-    else
-        ping -c 2 scale1.docker && /opt/spark-1.2.0-bin-hadoop2.4/sbin/start-slave.sh -h spark://scale1.docker:7077 ; 
-    fi 
+echo "running docker spark startup"
 
-### Kubernetes
+if [ $1 = "master" ] ; then 
+        /opt/spark-1.2.0-bin-hadoop2.4/sbin/start-master.sh ; 
+# slave
 else
-   
-    # One arg expected when launching kubernetes (master or slave)
-    if [ $1 = "slave" ]; then 
-       echo "START SLAVE pointing to ${SPARK_MASTER_SERVICE_HOST} " 
-       # SPARK_MASTER_SERVICE_HOST is 1.2.3.4:7077
-       /opt/spark-1.2.0-bin-hadoop2.4/sbin/start-slave.sh -h spark://${SPARK_MASTER_SERVICE_HOST}:7077
-    # master
-    else 
-       /opt/spark-1.2.0-bin-hadoop2.4/sbin/start-master.sh
-   
-    fi
+        ping -c 2 scale1.docker && /opt/spark-1.2.0-bin-hadoop2.4/sbin/start-slave.sh -h spark://scale1.docker:7077 ;  
 fi
 ### In all cases, we tail the logs as the final task...
 tailf /opt/spark-1.2.0-bin-hadoop2.4/logs/*
--- deploy/vagrant-run-up-test.sh
+++ deploy/vagrant-run-up-test.sh
@@ -18,9 +18,13 @@ vagrant destroy --force && vagrant up --no-parallel
 
 echo "RUNNING smoke tests..." 
 
-docker exec -i -t scale1 \
-/opt/spark-1.2.0-bin-hadoop2.4/bin/spark-submit \
---class org.apache.spark.examples.SparkPi \
---master spark://scale1.docker:7077 /scale-shared/spark-examples_2.10-1.1.1.jar 10000
+
+if `sleep 2 && docker ps | grep -q scale1`; then
+    docker exec -i -t scale1 /opt/spark-1.2.0-bin-hadoop2.4/bin/spark-submit --class org.apache.spark.examples.SparkPi --master spark://scale1.docker:7077 /scale-shared/spark-examples_2.10-1.1.1.jar 10000
+else
+    echo "FAILED: Looks like vagrant creation of the spark containers failed ..."
+    docker ps
+fi
+
 
 echo "DONE TESTING .  RESULTS ^ " 
